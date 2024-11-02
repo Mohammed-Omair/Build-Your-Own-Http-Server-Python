8f30a9a53a2a45b76e63ed0380d37c1def4e1a52
Mohammed Omair Mohiuddin <mohammedomair07@gmail.com>
2024-11-02 12:51:06 -0500

    Added Response Functionality

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ def main():
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     client_socket, client_address = server_socket.accept() # wait for client
     request = client_socket.recv(1024)
-    response = b"HTTP/1.1 200 OK\r\n"
+    response = b"HTTP/1.1 200 OK\r\n\r\n"
     client_socket.send(response)
 
 if __name__ == "__main__":
