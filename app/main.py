8edb673686b8f70b5e9b68447ce80b2aeb088d76
Mohammed Omair Mohiuddin <mohammedomair07@gmail.com>
2024-11-02 12:49:41 -0500

    Added Response Functionality

--- app/main.py
+++ app/main.py
@@ -4,8 +4,10 @@ import socket  # noqa: F401
 def main():
     
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept() # wait for client
-
+    client_socket, client_address = server_socket.accept() # wait for client
+    request = client_socket.recv(1024)
+    response = b"HTTP/1.1 200 OK\r\n"
+    client_socket.send(response)
 
 if __name__ == "__main__":
     main()
