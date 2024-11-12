d8086557de1ac6d8ad8adc100bd2d5b76bb8c1e1
Mohammed Omair Mohiuddin <mohammedomair07@gmail.com>
2024-11-12 14:37:45 -0600

    Addedfunctionality for files endpoint

--- app/main.py
+++ app/main.py
@@ -1,16 +1,26 @@
 import socket  # noqa: F401
 import threading
+import sys
+import os
 
 def handle_client(client_socket):
     # Receive the client's request
     request = client_socket.recv(1024)
     # Process and respond to the client
-    response(client_socket, request)
+    args = sys.argv
+    response(client_socket, request, args)
     # Close the client connection
     client_socket.close()
 
-def response(client_socket, request):
+def file_details(file_path):
+    file_size = os.stat(file_path).st_size
+    with open(file_path, 'r') as file:
+        file_data = file.read()
+    return file_size, file_data
+
+def response(client_socket, request, args):
     decoded_request = request.decode("utf-8").split()
+    file_path = args[-1] + (decoded_request[1].split("/")[-1])
     if decoded_request[1] == '/':
         response = b"HTTP/1.1 200 OK\r\n\r\n"
     elif "echo" in decoded_request[1]:
@@ -20,6 +30,10 @@ def response(client_socket, request):
     elif "user-agent" in decoded_request[1]:
         user_agent_endpoint = decoded_request[-1]
         response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(user_agent_endpoint)}\r\n\r\n{user_agent_endpoint}"
+        response = response.encode("utf-8")
+    elif "files" in decoded_request[1] and os.path.exists(file_path):
+        file_size, file_data = file_details(file_path)
+        response = f"HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: {file_size}\r\n\r\n{file_data}"
         response = response.encode("utf-8")
     else:
         response = b"HTTP/1.1 404 Not Found\r\n\r\n"
